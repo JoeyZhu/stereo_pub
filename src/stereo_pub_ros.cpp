9792907dcef7d1c695bd8089b502e5bf1b844c67
joey <joey@joey-nb>
2017-08-27 22:11:03 +0800

    added image transport.

--- src/stereo_pub_ros.cpp
+++ src/stereo_pub_ros.cpp
@@ -8,7 +8,9 @@
 
 
 #include "ros/ros.h"
-
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 int main(int argc, char ** argv){
 
@@ -16,7 +18,17 @@ int main(int argc, char ** argv){
 	ros::NodeHandle n;
 	ros::Rate loop_rate(1);
 
+	  image_transport::ImageTransport it(n);
+	  image_transport::Publisher left_image_pub = it.advertise("camera/left/image", 1);
+	  image_transport::Publisher right_image_pub = it.advertise("camera/right/image", 1);
+
+
+
 	while(ros::ok()){
+
+		//todo: read image from camera3
+		sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+
 		ROS_INFO("hello");
 		ros::spinOnce();
 		loop_rate.sleep();
